91098dddcaf20a9143b0e17dd3f6bc358cd0bd92
sanjeeb42 <raisanjeeb42@gmail.com>
2025-08-17 21:50:51 +0530

    Adds Checks before proceeding & Few minor Improvements

--- Command-Design-Pattern/src/main/java/RemoteController/RemoteControl.java
+++ Command-Design-Pattern/src/main/java/RemoteController/RemoteControl.java
@@ -6,12 +6,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class RemoteControl {
+    private static final int MAX_COMMANDS = 4;
     List<ICommand>commands;
     List<Boolean>isTurnedon;
 
     public RemoteControl(){
-        commands=new ArrayList<>(4);
-        isTurnedon=new ArrayList<>(4);
+        commands=new ArrayList<>(MAX_COMMANDS);
+        isTurnedon=new ArrayList<>(MAX_COMMANDS);
         for(int i=0;i<4;i++){
             commands.add(null);
             isTurnedon.add(false);
@@ -19,13 +20,22 @@ public class RemoteControl {
     }
 
     public void assignCommands(int index,ICommand command){
-        if(index>=0 && index<4){
+        if(index>=0 && index<MAX_COMMANDS){
             commands.set(index,command);
             isTurnedon.set(index,false);
         }
     }
 
     public void pressButton(int index){
+        if(index<0 ||index>=MAX_COMMANDS){
+            System.out.println("Index outside of Range , Please check again");
+            return;
+        }
+        ICommand command = commands.get(index);
+        if (command == null) {
+            // Optionally, log or handle the case where no command is assigned
+            return;
+        }
         if(isTurnedon.get(index)){
             commands.get(index).undo();
             isTurnedon.set(index,false);
