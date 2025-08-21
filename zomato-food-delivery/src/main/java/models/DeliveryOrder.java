ffb075aeade10612433049bdfb07c5c530162b76
sanjeeb42 <raisanjeeb42@gmail.com>
2025-08-21 23:17:58 +0530

    Adds remaining model files and also add PaymentStrategy

---
+++ zomato-food-delivery/src/main/java/models/DeliveryOrder.java
@@ -0,0 +1,22 @@
+package models;
+
+public class DeliveryOrder extends Order{
+    private String userAddress;
+    public DeliveryOrder() {
+        userAddress = "";
+    }
+
+    @Override
+    public String getType() {
+        return "Delivery";
+    }
+
+    public void setUserAddress(String addr) {
+        userAddress = addr;
+    }
+
+    public String getUserAddress() {
+        return userAddress;
+    }
+
+}
